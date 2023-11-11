6f24e7e5548bfca22505be1454e348267c919b4d
keithprimas <136135449+keithprimas@users.noreply.github.com>
2023-11-11 13:56:19 -0500

    added utils helpers and controller/api routes

---
+++ utils/helpers.js
@@ -0,0 +1,13 @@
+module.exports = {
+    format_date: (date) => {
+      return date.toLocaleDateString();
+    },
+  
+    format_amount: (amount) => {
+      return parseInt(amount).toLocaleString();
+    },
+  
+    is_my_page: (pageUser, userId) => {
+      return pageUser === userId;
+    },
+  };
