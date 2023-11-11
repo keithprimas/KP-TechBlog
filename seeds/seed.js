0b3325facbe9e9b3c9198496c48620cfa40052ed
keithprimas <136135449+keithprimas@users.noreply.github.com>
2023-11-11 14:15:52 -0500

    added database seeds data

---
+++ seeds/seed.js
@@ -0,0 +1,29 @@
+const sequelize = require("../config/connection");
+const { User, BlogPost, Comment } = require("../model");
+
+const userData = require("./userData.json");
+const blogPostData = require("./blogPostData.json");
+const commentData = require("./commentData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blogPost of blogPostData) {
+    await BlogPost.create({
+      ...blogPost,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  const comments = await Comment.bulkCreate(commentData);
+
+  process.exit(0);
+
+};
+
+seedDatabase();
